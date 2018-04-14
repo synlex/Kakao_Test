4eb8afa5d351884d46c591a5973f8a9ba47820b1
synlex <synlex@gmail.com>
2018-04-14 12:57:31 +0900

    Initial Commit

---
+++ Kakao_Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kakao_Test
+//
+//  Created by synlex on 2018. 4. 14..
+//  Copyright © 2018년 synlex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
